5d4bbf506a52268d3b098a50e66b01b8678d93c5
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-27 17:27:57 +0530

    Class Method to compute EmployeeWage

---
+++ UC7EmployeeWageClass/Neww.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC7EmployeeWageClass
+{
+    class Neww
+    {
+        public int getEmpWage(int hours)
+        {
+            int empWage = 0;
+            const int empRate = 20;
+            int totalWage = 0;
+            empWage = hours * empRate;
+            totalWage += empWage;
+            return totalWage;
+        }
+
+        public int getHours(int empCheck)
+        {
+            const int fullTime = 2;
+            const int partTime = 1;
+            const int totalDays = 20;
+            int hours = 0;
+            for (int day = 0; day < totalDays; day++)
+            {
+                switch (empCheck)
+                {
+                    case fullTime:
+                        hours += 12;
+                        break;
+                    case partTime:
+                        hours += 8;
+                        break;
+                    default:
+                        hours += 0;
+                        break;
+                }
+
+            }
+            return hours;
+        }
+    }
+}
---
+++ UC7EmployeeWageClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC7EmployeeWageClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = 0;
+            Neww obj = new Neww();
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            hours = obj.getHours(empCheck);
+            Console.WriteLine("Total EmployeeWages is : " + obj.getEmpWage(hours));
+        }
+    }
+}
