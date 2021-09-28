598840a3a228d5347bcd9f0cd5832ba959dde78f
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-28 16:26:32 +0530

    Hemanth :Finding Employee Wage

---
+++ UC2ObjEmpWage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UC2ObjEmpWage
+{
+    class Program
+    {
+        public void getEmpWage()
+        {
+            int fullTime = 1;
+            int empRate = 20;
+            int empWage = 0;
+            int hours = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == fullTime)
+                hours = 8;
+            else
+                hours = 0;
+
+            empWage = empRate * hours;
+            Console.WriteLine("Employee Wage :" + empWage);
+        }
+        static void Main(string[] args)
+        {
+            Program ob = new Program();
+            ob.getEmpWage();
+        }
+    }
+}
