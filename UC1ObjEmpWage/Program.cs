9a298805f4c6056ce52b6f2cb2141f7f1896710e
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-28 16:24:19 +0530

    Hemanth:Finding Employee Attendance

---
+++ UC1ObjEmpWage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UC1ObjEmpWage
+{
+    class Program
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 1)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+        static void Main(string[] args)
+        {
+            Program ob = new Program();
+            ob.Attendance();
+        }
+    }
+}
